1f9b69551da1207468ae6f8c654906391097836e
ailuroq <yaroslav_ovsyannikov@list.ru>
2021-05-09 20:47:56 +0300

    references

--- src/controllers/Work.js
+++ src/controllers/Work.js
@@ -1,14 +1,14 @@
 const pool = require('../database/pool');
 
 exports.getAllWorks = async () => {
-    const getAllWorksQuery = 'select * from works order by id asc';
+    const getAllWorksQuery = 'select * from work order by id asc';
     const queryResult = await pool.query(getAllWorksQuery);
     const works = queryResult.rows[0];
     return {works};
 };
 
 exports.createWork = async (name) => {
-    const createWorkQuery = 'insert into works(name) values($1)';
+    const createWorkQuery = 'insert into work(name) values($1)';
     const queryResult = await pool.query(createWorkQuery, [name]);
     const work = queryResult.rows[0];
     return {work};
@@ -35,7 +35,7 @@ exports.getPotentialCountryDataToDelete = async (id) => {
 };
 
 exports.updateWork = async (id, name) => {
-    const updateWorkQuery = 'update works\n' +
+    const updateWorkQuery = 'update work\n' +
         'set name=$1 where id=$2';
     const queryResult = await pool.query(updateWorkQuery, [id, name]);
     const updatedWork = queryResult.rows[0];
